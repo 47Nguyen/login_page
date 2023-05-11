20db3b9ef4e31c67a0114517da9e428da9adde12
47Nguyen <ankhanhthinj@gmail.com>
2023-05-11 22:51:37 +0700

    first

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path')
+
+
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine','ejs')
 app.use(express.urlencoded({extended:true}))
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 const { default: mongoose } = require('mongoose')
 mongoose.connect('mongodb+srv://trungluong0806:Lacussaber080699@cluster0.w8zcmxn.mongodb.net/?retryWrites=true&w=majority')
@@ -15,6 +19,7 @@ const userSchema = new mongoose.Schema({
     email: String,
     Password: String
 })
+
 const User = mongoose.model('User', userSchema)
 
 app.post('/user-registration-form', (req, res)=>{
@@ -32,6 +37,10 @@ app.post('/shipper-registration-form', (req, res)=>{
     console.log(`Email: ${req.body.email}`)
     console.log(`National ID: ${req.body.national}`)
     res.send("Post form submitted")
+})
+
+app.get('/login', (req, res) =>{
+    res.render('login.ejs')
 })
 
 /*
