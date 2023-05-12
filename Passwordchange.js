5f8d8d62f3c70c4f0eb091139b7eed9eddc26ccb
s397976 <116806009+s397976@users.noreply.github.com>
2023-05-12 16:44:26 +0700

    Create Passwordchange.js

---
+++ Passwordchange.js
@@ -0,0 +1,43 @@
+var  myInput=document.getElementByName('passord');
+var letter=document.getElementByName('letter');
+var capital=document.getElementsByName('number')
+myInput.onlkeyup=function{
+  var lowecaseletters=/[a-z]/g
+  if (myInput.value.match(lowecaseletters)){
+    letter.classList.remove('invalid')
+    letter.classList.add('valid')
+       } else{
+    letter.classList.remove('valid')
+    letter.classList.add('invalid')
+    }
+  var upcaseletters=/[A-Z]/g
+  if (myInput.value.match(upcaseletters)){
+    letter.classList.remove('invalid')
+    letter.classList.add('valid')
+       } else{
+    letter.classList.remove('valid')
+    letter.classList.add('invalid')
+    }
+  var numbers=/[0-9]/g
+  if (myInput.value.match(numbers)){
+    letter.classList.remove('invalid')
+    letter.classList.add('valid')
+       } else{
+    letter.classList.remove('valid')
+    letter.classList.add('invalid')
+    }
+  if (myInput.value.length>=8){letter.classList.remove('invalid')
+    letter.classList.add('valid')
+       } else{
+    letter.classList.remove('valid')
+    letter.classList.add('invalid')
+    }
+  if (myInput.vaule.length<=20){
+    letter.classList.remove('invalid')
+    letter.classList.add('valid')
+       } else{
+    letter.classList.remove('valid')
+    letter.classList.add('invalid')
+  }
+  }
+  
